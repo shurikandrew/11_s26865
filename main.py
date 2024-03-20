467237e780f3b82ba913dda736b522c1e9ea79b4
shurikandrew <s26865@pjwstk.edu.pl>
2024-03-20 18:58:17 +0100

    Task 2: Functions solved

--- main.py
+++ main.py
@@ -4,3 +4,12 @@
 
 squares_list = [number**2 for number in range(1,11)]
 print("List of squares: ",squares_list)
+
+#Task 2: Functions
+#Expand the previous program by defining a function that takes a range of numbers as input
+#and returns a list of squares for that range.e_squares(start,end))
+
+def e_squares(start, end):
+    return [number**2 for number in range(start,end+1)]
+
+print("List from e_squares function: ",e_squares(3,20))
